8fd94afdb687ec70e267072dc5f0a5d0bc85e0e1
Linus.Young <Linus.Young@yaocheng.cn>
2023-05-06 13:02:30 +0800

    master-push

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+# Press the green button in the gutter to run the script
 if __name__ == '__main__':
     print_hi('PyChaegghrm-hotfix-mastermodify')
+    print_hi('PyChaegghrm-hotfix-hotfixmodify')
     print_hi('PyChaegghrm-hotfix-hotfixmodify')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
