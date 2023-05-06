e7b65c5e3b7d081701ae9d501fcf72b8bff76230
Linus.Young <Linus.Young@yaocheng.cn>
2023-05-06 12:47:40 +0800

    conflix-master

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyChaegghrm-hotfix')
+    print_hi('PyChaegghrm-hotfix-mastermodify')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
